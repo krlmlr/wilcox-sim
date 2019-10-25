510e5594973b06bd47d1a8c58d70033cbf84e909
Kirill Müller <krlmlr+r@mailbox.org>
2019-10-25 21:47:23 +0200

    Visualize, show summary statistics

--- 01-sim.R
+++ 01-sim.R
@@ -8,14 +8,16 @@ control <- rnorm(N, -10, 70)
 music <- rnorm(N, 10, 40)
 speech <- rnorm(N, 15, 100)
 
+#' Wir erzeugen uns ein data frame.
+
 control_tbl <- tibble(dd = control, type = "control")
 music_tbl <- tibble(dd = music, type = "music")
 speech_tbl <- tibble(dd = speech, type = "speech")
 
 tbl <- bind_rows(control_tbl, music_tbl, speech_tbl)
 
-# ggplot(tbl, aes(x = type, y = dd)) +
-#   geom_boxplot()
+ggplot(tbl, aes(x = type, y = dd)) +
+  geom_boxplot()
 
 wilcox.test(control, alternative = "less")
 wilcox.test(music, alternative = "greater")
@@ -24,3 +26,8 @@ wilcox.test(speech, alternative = "greater")
 wilcox.test(control, music, alternative = "less")
 wilcox.test(control, speech, alternative = "less")
 wilcox.test(music, speech)
+
+tbl %>%
+  group_by(type) %>%
+  summarize(median(dd), mean(dd), sd(dd)) %>%
+  ungroup()
